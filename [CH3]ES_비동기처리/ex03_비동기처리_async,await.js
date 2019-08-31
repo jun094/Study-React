da0e55fac666b8809650e2411987e625a92410a2
JunYoung <jun094@naver.com>
2019-08-31 17:41:36 +0900

    ES 비동기처리

--- [CH3]ES_비동기처리/ex03_비동기처리_async,await.js
+++ [CH3]ES_비동기처리/ex03_비동기처리_async,await.js
@@ -9,3 +9,39 @@ async function process() {
 }
 
 process();
+
+///////////////////////////////////////////////////////////////////////////////
+function sleep(ms) {
+  return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+async function process() {
+  console.log('안녕하세요!');
+  await sleep(1000); // 1초쉬고
+  console.log('반갑습니다!');
+}
+
+process().then(() => {
+  console.log('작업이 끝났어요!');
+});
+
+//////////////////////////////////////////////////////////////////////////////
+function sleep(ms) {
+  return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+async function makeError() {
+  await sleep(1000);
+  const error = new Error();
+  throw error;
+}
+
+async function process() {
+  try {
+    await makeError();
+  } catch (e) {
+    console.error(e);
+  }
+}
+
+process();
