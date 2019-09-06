048474923ef808a7773368b6c9bf0d2e152876d1
JunYoung <jun094@naver.com>
2019-09-06 19:13:12 +0900

    React 입문

--- [CH4]React_tutorial/begin-react/src/App.js
+++ [CH4]React_tutorial/begin-react/src/App.js
@@ -1,9 +1,67 @@
-import React from 'react';
+import React, { useRef, useState } from 'react';
 import UserList from './UserList';
+import CreateUser from './CreateUser';
 
 function App() {
+  const [inputs, setInputs] = useState({
+    username : '',
+    email : '',
+  });
+  const { username, email } = inputs;
+  const onChange = e => {
+    const { name, value } = e.target;
+    setInputs({
+      ...inputs,
+      [name]: value
+    });
+  };
+  const [users, setUsers] = useState([
+    {
+      id: 1,
+      username: 'velopert',
+      email: 'public.velopert@gmail.com'
+    },
+    {
+      id: 2,
+      username: 'tester',
+      email: 'tester@example.com'
+    },
+    {
+      id: 3,
+      username: 'liz',
+      email: 'liz@example.com'
+    }
+  ]);
+
+
+  const nextId = useRef(4);
+
+  const onCreate = () => {
+    const user = {
+      id: nextId.current,
+      username,
+      email
+    };
+    //setUsers([...users, user]); // spread연산자 사용하는 방법
+    setUsers(users.concat(user)); //concat을 이용한 다른 방법
+    // push를 사용하면 불변성을 지키기 못함 !!!
+
+    setInputs({
+     username: '',
+     email: ''
+   });
+    nextId.current += 1;
+  };
   return (
-    <UserList />
+    <>
+      <CreateUser
+        username={username}
+        email={email}
+        onChange={onChange}
+        onCreate={onCreate}
+      />
+      <UserList users={users} />
+    </>
   );
 }
 
---
+++ [CH4]React_tutorial/begin-react/src/CreateUser.js
@@ -0,0 +1,26 @@
+// 배열에 새로운 항목 추가하기
+
+import React from 'react';
+
+// oncreate는 함수 !
+function CreateUser({ username, email, onChange, onCreate }) {
+  return (
+    <div>
+      <input
+        name="username"
+        placeholder="계정명"
+        onChange={onChange}
+        value={username}
+      />
+      <input
+        name="email"
+        placeholder="이메일"
+        onChange={onChange}
+        value={email}
+      />
+      <button onClick={onCreate}>등록</button>
+    </div>
+  );
+}
+
+export default CreateUser;
