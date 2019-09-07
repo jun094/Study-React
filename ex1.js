66755d6edbabd0115283f6b3dad8dd30ad5a6aa5
JunYoung <jun094@naver.com>
2019-09-07 17:33:22 +0900

    ex

---
+++ ex1.js
@@ -0,0 +1,28 @@
+//단위수 측정
+var len = s.length;
+var rank;
+if (len / 2 == 0) {
+  rank = len / 2;
+} else {
+  len--;
+  rank = len / 2;
+}
+
+
+function solution(s) {
+  var char=0;
+  var len = s.length;
+
+  for (var i = 0; i < s.length; i++) {
+    if (s[i] == s[i + 1]) {
+      len--;
+
+    }
+  }
+  var answer = len;
+
+  return answer;
+}
+
+console.log(solution('aabbaccc'));
+2a2ba3c
