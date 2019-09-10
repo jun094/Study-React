0eb6e44836f9bbe13b0fa3be48b9503e92459edd
JunYoung <jun094@naver.com>
2019-09-10 14:42:59 +0900

    React 입문

--- [CH4]React_tutorial/begin-react/src/ex/ex02_props.js
+++ [CH4]React_tutorial/begin-react/src/ex/ex02_props.js
@@ -1,5 +1,6 @@
 // props  : . props 는 properties 의 줄임말입니다.
 // 우리가 어떠한 값을 컴포넌트에게 전달해줘야 할 때, props 를 사용합니다.
+
 import React from 'react';
 import Hello from './Hello';
 import Wrapper from './Wrapper';
