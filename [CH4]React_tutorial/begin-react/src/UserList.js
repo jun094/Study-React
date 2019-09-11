2448508b15af80070499a34dfe7822a6dd607e8c
JunYoung <jun094@naver.com>
2019-09-11 13:48:27 +0900

    React 입문

--- [CH4]React_tutorial/begin-react/src/UserList.js
+++ [CH4]React_tutorial/begin-react/src/UserList.js
@@ -7,15 +7,37 @@ import React, {useEffect } from 'react';
 function User({ user, onRemove, onToggle }) {
   const { username, email, id, active } = user // 비구조 할당
 
+  /*
+  // 1. 배열이 비어져 있는경우 -- [] 배열은 deps라고 부름
   useEffect(() => {
     console.log('컴포넌트가 화면에 나타남'); // 마운트 될 때 ( 등록버튼 누를 떄 )
     // 화면에 나타나고 다시 부를고 싶을 때 사용
-    // ex) setTime, API 재호출
+    // ex) setTime, API 재호출, 라이브러리 사용할 때
 
     return () => {
       console.log('컴포넌트가 화면에서 사라짐'); // 언마운트 될 때 ( 삭제 버튼 눌를 때 )
+      //ex) 라이브러리 인스턴스 제거
     };
   }, []);
+  */
+
+  /*
+  // 2. 배열에 값이 있는 경우 ( ex) 배열에 user 가 았는경우 )
+  useEffect(()=> {
+    console.log("user값이 설정됨");
+    console.log(user);
+    return () => {
+      console.log("user값이 바뀌기 전");
+      console.log(user);
+    }
+  }, [user]);
+  */
+
+
+  // 3. 배열(deps)이 아예 없는 경우
+  useEffect(()=> {
+    console.log(user);
+  })
 
 
   return (
