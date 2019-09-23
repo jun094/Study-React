cc10020857a2dab56ca8728358f74c1f813a6f00
JunYoung <jun094@naver.com>
2019-09-23 17:35:05 +0900

    React 스타일링 - styled-components

---
+++ CH5_React_styling/styling-with-styled-components/src/App.js
@@ -0,0 +1,90 @@
+import React from "react";
+import styled, { ThemeProvider } from "styled-components";
+import Button from "./components/Button";
+
+/*
+const Circle = styled.div`
+  width: 5rem;
+  height: 5rem;
+  background: ${props => props.color };
+  border-radius: 50%;
+  ${props => props.huge && css`width : 10rem; height: 10rem;`}
+`;
+// props.huge && ...  : huge값이 들어왔다면 
+*/
+
+const AppBlock = styled.div`
+  width: 512px;
+  margin: 0 auto;
+  margin-top: 4rem;
+  border: 1px solid black;
+  padding: 1rem;
+`;
+
+const ButtonGroup = styled.div`
+  & + & {
+    margin-top: 1rem;
+  }
+`;
+
+const palette = {
+  blue: "#228be6",
+  gray: "#495057",
+  pink: "#f06595"
+};
+
+function App() {
+  return (
+    <ThemeProvider theme={{ palette }}>
+      <AppBlock>
+        <ButtonGroup>
+          <Button size="large">BUTTON</Button>
+          <Button>BUTTON</Button>
+          <Button size="small">BUTTON</Button>
+        </ButtonGroup>
+        <ButtonGroup>
+          <Button color="gray" size="large">
+            BUTTON
+          </Button>
+          <Button color="gray">BUTTON</Button>
+          <Button color="gray" size="small">
+            BUTTON
+          </Button>
+        </ButtonGroup>
+        <ButtonGroup>
+          <Button color="pink" size="large">
+            BUTTON
+          </Button>
+          <Button color="pink">BUTTON</Button>
+          <Button color="pink" size="small">
+            BUTTON
+          </Button>
+        </ButtonGroup>
+        <ButtonGroup>
+          <Button size="large" outline>
+            BUTTON
+          </Button>
+          <Button color="gray" outline>
+            BUTTON
+          </Button>
+          <Button color="pink" size="small" outline>
+            BUTTON
+          </Button>
+        </ButtonGroup>
+        <ButtonGroup>
+          <Button size="large" fullWidth>
+            BUTTON
+          </Button>
+          <Button size="large" color="gray" fullWidth>
+            BUTTON
+          </Button>
+          <Button size="large" color="pink" fullWidth>
+            BUTTON
+          </Button>
+        </ButtonGroup>
+      </AppBlock>
+    </ThemeProvider>
+  );
+}
+
+export default App;
---
+++ CH5_React_styling/styling-with-styled-components/src/components/Button.js
@@ -0,0 +1,91 @@
+import React from "react";
+import styled, { css } from "styled-components";
+import { darken, lighten } from "polished";
+
+//비구조 할당
+const colorStyles = css`
+  ${({ theme, color }) => {
+    const selected = theme.palette[color];
+    return css`
+      background: ${selected};
+      &:hover {
+        background: ${lighten(0.1, selected)};
+      }
+      &:active {
+        background: ${darken(0.1, selected)};
+      }
+      ${props =>
+        props.outline &&
+        css`
+          color: ${selected};
+          background: none;
+          border: 1px solid ${selected};
+          &:hover {
+            background: ${selected};
+            color: white;
+          }
+        `}
+    `;
+  }}
+`;
+
+const sizes = {
+  large: {
+    height: "3rem",
+    fontSize: "1.25rem"
+  },
+  medium: {
+    height: "2.25rem",
+    fontSize: "1rem"
+  },
+  small: {
+    height: "1.75rem",
+    fontSize: "0.875rem"
+  }
+};
+
+//비구조할당
+const sizeStyles = css`
+  ${({ size }) => css`
+    height: ${sizes[size].height};
+    font-size: ${sizes[size].fontSize};
+  `}
+`;
+
+const StyledButton = styled.button`
+  /* 공통 스타일 */
+  display: inline-flex;
+  outline: none;
+  border: none;
+  border-radius: 4px;
+  color: white;
+  font-weight: bold;
+  cursor: pointer;
+  padding-left: 1rem;
+  padding-right: 1rem;
+
+  /* 크기 */
+  ${sizeStyles}
+
+  /* 색상 */
+  ${colorStyles}
+
+  /* 기타 */
+  & + & {
+    margin-left: 1rem;
+  }
+`;
+
+function Button({ children, color, outline, size, ...rest }) {
+  return (
+    <StyledButton color={color} size={size} outline={outline} {...rest}>
+      {children}
+    </StyledButton>
+  );
+}
+
+Button.defaultProps = {
+  color: "blue",
+  size: "medium"
+};
+export default Button;
